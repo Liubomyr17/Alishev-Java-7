fffff241e70dc26ddf97114184cc701e52db464d
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-09-20 23:32:46 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+ Scanner s = new Scanner(System.in);
+        System.out.println("Enter some number:");
+        // String string = s.nextLine();
+        int x = s.nextInt();
+        System.out.println("You entered: " + x); // + string
+    }
+}
